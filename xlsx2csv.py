ca1bd6b3ea6d9fd90ea8b3bc9705892fbe26fcfd
Ed Lui <rewq142857@hotmail.com>
2019-08-26 16:42:01 +0800

    Update xlsx2csv.py

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -1,8 +1,15 @@
 import pandas as pd
-map=pd.read_excel('JPM fund')
+map=pd.read_excel('JPM fund.xlsx')
+#first=pd.read_csv('first.csv')
 files=[]
+files.append('JPMAM-HistoricalPrice(LU0945454980).xlsx')
 for file in files:
-  df=pd.read_excel(file,skiprows=5)[['Date','Nav']]
-  df.rename({'Nav':map[map['ISIN']==file[-11:-1]]['SecId']})
-  first.join(df.set_index('Date'), on='Date')
-first.to_csv('final',index=False)
+  df=pd.read_excel(file,skiprows=6)
+  print(df)
+  df=df[['Date','Nav']]
+  print(file[-13-5:-1-5])
+  SecId=map[map['ISIN']==file[-13-5:-1-5]]['SecId'].item()
+  df=df.rename(columns={'Nav':SecId})
+  df.to_csv('first.csv',index=False)
+  #first.join(df.set_index('Date'), on='Date')
+#first.to_csv('final.csv',index=False)
